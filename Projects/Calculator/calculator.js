ca5ae1c45f4498904795f5b4229c08e5ab7a2ebb
Sahil Mina <sahilmina55555@gmail.com>
2025-03-09 18:03:45 +0530

    added

---
+++ Projects/Calculator/calculator.js
@@ -0,0 +1,42 @@
+<<<<<<< HEAD
+let currentDisplay = '';
+document.querySelector('#display').value = currentDisplay;
+
+
+                      // Animation for the title
+
+
+    window.onload = function () {
+let title = document.getElementById("animated-title");
+let text = title.innerText;
+title.innerHTML = ""; 
+
+for (let i = 0; i < text.length; i++) {
+let span = document.createElement("span");
+span.innerText = text[i];
+span.style.animationDelay = i * 0.1 + "s";
+span.classList.add("animate-letter");
+title.appendChild(span);
+}
+=======
+let currentDisplay = '';
+document.querySelector('#display').value = currentDisplay;
+
+
+                      // Animation for the title
+
+
+    window.onload = function () {
+let title = document.getElementById("animated-title");
+let text = title.innerText;
+title.innerHTML = ""; 
+
+for (let i = 0; i < text.length; i++) {
+let span = document.createElement("span");
+span.innerText = text[i];
+span.style.animationDelay = i * 0.1 + "s";
+span.classList.add("animate-letter");
+title.appendChild(span);
+}
+>>>>>>> 14b960ae6fb5ae2b11d028430dd301ca2c6010cf
+};
